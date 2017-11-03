b60407f5c0df6b24b6344c07917d488020513945
zhoujie <zhoujiejay@didichuxing.com>
2017-11-03 11:45:14 +0800

    add init data

--- reducers/decks.js
+++ reducers/decks.js
@@ -1,6 +1,29 @@
 import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions/types'
 
-function decks ( state = {}, action ) {
+function decks ( state = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}, action ) {
 
   switch (action.type) {
     case RECEIVE_DECKS:
